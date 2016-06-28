2953e6fa6ecd9a2782687267be1eaddb72fb6ac1
Brian Duggan <brian@promptworks.com>
2016-06-28 10:06:53 -0400

    no mos

--- lib/Slang/Mosdef.pm
+++ lib/Slang/Mosdef.pm
@@ -4,7 +4,7 @@ sub EXPORT(|) {
 
   role MOSDEF::Grammar {
     rule routine_declarator:sym<sub> { [ 'sub' | 'lambda' | 'λ' ] <routine_def('sub')> }
-    rule routine_declarator:sym<method> { [ 'def' | 'method' | 'mos' ] <method_def('method')> }
+    rule routine_declarator:sym<method> { [ 'def' | 'method' ] <method_def('method')> }
   }
 
   nqp::bindkey(%*LANG, 'MAIN', %*LANG<MAIN>.HOW.mixin(%*LANG<MAIN>, MOSDEF::Grammar));
